9bfd2d5197706677f7ab35b7f812f2f1839a0216
Eyetsko <104879063+Eyetsko@users.noreply.github.com>
2022-11-06 13:54:16 -0500

    added link to github code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello!</h1>
+      <footer>
+        <div>
+          Coded by Emily Yetsko and is{" "}
+          <a href="https://github.com/Eyetsko/test">open-sourced</a>
+        </div>
+      </footer>
+    </div>
+  );
 }
